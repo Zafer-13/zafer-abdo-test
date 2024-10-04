0c9cb47b77b57b80ecab7f331a2fcf323fcb7052
Zafer <zaferabdo85@gmail.com>
2024-10-05 00:50:31 +0300

    fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,6 +4,7 @@ import Switcher from "../switcher/Switcher";
 import styles from "./header.module.scss";
 const Header = () => {
   const { t } = useTranslation("home");
+
   return (
     <div className={styles.container}>
       <h1>{t("page")}</h1>
